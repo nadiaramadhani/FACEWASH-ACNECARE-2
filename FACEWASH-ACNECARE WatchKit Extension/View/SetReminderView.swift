e4ff6ee069101d8038e70e77e9780731b28d065b
Nadia Ramadhani <103446529+twicetw@users.noreply.github.com>
2022-07-19 10:11:22 +0700

    Installing- Package- 19Juli

--- FACEWASH-ACNECARE WatchKit Extension/View/SetReminderView.swift
+++ FACEWASH-ACNECARE WatchKit Extension/View/SetReminderView.swift
@@ -9,53 +9,6 @@ import SwiftUI
 import WatchDatePicker
 
 
-struct MainSettingView : View {
-    @State var changeTime = false
-    @State var voiceOver = false
-    @StateObject var notifManager = LocalNotificationManager()
-    @State var selected = Date()
-    @Environment(\.scenePhase) var scenePhase
-
-    var body: some View {
-        
-        VStack{
-            ZStack{
-                NavigationLink("", destination: SetReminderView()
-                    .environmentObject(notifManager)
-                               , isActive: $changeTime)
-                    .opacity(0)
-            Button(action: {
-                changeTime.toggle()
-                
-            }, label: {
-                HStack{
-                    Text("Set reminder")
-                    Spacer()
-                    
-                }
-            })
-                
-            }
-            
-            Button(action: {
-                if voiceOver{
-                }else {
-                    AVService.shared.player?.stop()
-                }
-                
-            }, label: {
-                HStack{
-                    Toggle("Voice over", isOn: $voiceOver)
-                }
-            })
-            Spacer()
-
-            
-        }.navigationTitle("Settings")
-            .navigationBarTitleDisplayMode(.inline)
-    }
-}
-
 
 struct SetReminderView: View {
     @State private var isMorningPicker = false
